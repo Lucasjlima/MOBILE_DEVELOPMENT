cadae834d563f116d30126a04a95f242e8d36fc0
Lucas <lucasjlima2006@gmail.com>
2025-08-20 08:55:06 -0300

    first commit

---
+++ welcome-app/App.tsx
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.warning} >Welcome to React Native!</Text>
+      <Text style={styles.title} >This is my first aplication using React Native</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'purple',
+    alignItems: 'center',
+    justifyContent: 'center',
+  
+  },
+  warning : {
+    fontSize : 24,
+    color : "blue",
+    fontWeight : "bold"
+  },
+
+  title : {
+    fontSize : 25,
+    textAlign : "center",
+    color : "yellow"
+  }
+});
+
+
+
