2b71b7bd74a5d63ff96323ef1c4579175714f9ae
Lucas <lucasjlima2006@gmail.com>
2025-08-20 09:50:47 -0300

    info : commit

--- welcome-app/App.tsx
+++ welcome-app/App.tsx
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <TextInput placeholder='input your name' style={styles.input}
+      />
       <Text style={styles.name} >Lucas José!</Text>
       <Text style={styles.warning} >Welcome to React Native!</Text>
-      <Text style={styles.title} >This is my first aplication using React Native</Text>
+      <Text style={styles.title} >This is my first application using React Native</Text>
+      <Image 
+        source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+        style={styles.image}
+        />
+        <Image 
+        source={require("./assets/cloud-sad.webp")}
+        style={styles.image}
+        />
+        <Button title='Click Me!' onPress={() => alert("Button has been clicked!")}
+        />
+        
+        <Text  style={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae non reprehenderit cum. Aperiam consectetur expedita voluptatum. Cum ea libero, repudiandae ipsa atque nam, officiis saepe, in rem illum similique reiciendis.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae non reprehenderit cum. Aperiam consectetur expedita voluptatum. Cum ea libero, repudiandae ipsa atque nam, officiis saepe, in rem illum similique reiciendis.
+        </Text>
+        
       <StatusBar style="auto" />
     </View>
   );
@@ -34,7 +52,29 @@ const styles = StyleSheet.create({
   name : {
     color : "#fff",
     fontSize : 20
+  },
+  image : {
+    width: 100,
+    height : 100,
+    borderRadius : 10,
+    margin : 10
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+  },
+  text : {
+    textAlign : "justify",
+    margin : 20,
+    color : "black"
   }
+
 });
 
 
